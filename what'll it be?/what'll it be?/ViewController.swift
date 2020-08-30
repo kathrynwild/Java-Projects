f63914876e80eca0081019cc5fb423b0528053c9
katwild <kathryn-wild@uiowa.edu>
2020-08-30 10:14:29 -0500

    All projects

---
+++ what'll it be?/what'll it be?/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  what'll it be?
+//
+//  Created by KatWild on 8/26/20.
+//  Copyright © 2020 KatWild. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    //MARKS: Actions
+    @IBAction func guessOrderButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func placeOrderButton(_ sender: UIButton) {
+    }
+    
+    //MARKS: Stacks
+    @IBOutlet weak var guessOrderStack: UIStackView!
+    
+}
+
